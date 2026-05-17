6757cf56fdb7b7bd9661265a15fccc1912a77a9a
Sonu C <sonuchandru2004@gmail.com>
2026-05-17 17:57:33 +0530

    Add files via upload

---
+++ Programming.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Programming {
+    public static void main(String[] args) {
+//        ArrayList al = new ArrayList();
+//      String[] Al = new String[20];
+//        for(int i=4;i>=1;i--){
+//            al.add("*");
+//        }
+//        for(int j=1;j<=4;j++){
+//            al.add("*");
+//        }
+//        System.out.println(al);
+//        for(int i=1;i<=5;i++){
+//            System.out.println(" ".repeat(5-i)+" *".repeat(i));
+//        }
+//        for(int i=1;i<=10;i++){
+//            System.out.println("* ".repeat(i));
+//        }
+//        int a = 2;
+//        int b = 5;
+//        System.out.println(a*b/a+1);
+//
+//        boolean x = true;
+//        boolean y = false;
+//        System.out.println(x && y);
+
+//        int x = 10;
+//        int y = 6;
+//        int res = x++ + ++y + --y + x-- + y++;
+//        System.out.println(res);
+
+//        int x = 10;
+//        x *= 2;
+//        System.out.println(x);
+
+//        float f = (1/4)*10;
+//        System.out.println(f);
+
+//        int x = 001, y = 010, z = 100;
+//        int i = --x + y++ - z-- - --z + ++y - --x + y-- - --x;
+//        System.out.println(x);
+//        System.out.println(y);
+//        System.out.println(z);
+//        System.out.println(i);
+
+//        double x = 10.0;
+//        short y = 10;
+//        System.out.println(x==y);
+
+//        int x = 5;
+//        int y = 20;
+//        System.out.println((x>3)&&(y<15));
+
+//        int x = 10;
+//        x = x++;
+//        System.out.println(x);
+
+//         int i = 3;
+//        int x = 5;
+//        System.out.println("i :" + i + " x :" + x );
+
+//        int ch  = 'A';
+//        System.out.println(ch);
+
+//       Scanner scan = new Scanner(System.in);
+//       int n = scan.nextInt();
+//       for(int i=1;i<=n;i++){
+//           for(int j=1;j<=n;j++){
+//               System.out.print("*");
+//           }
+//           System.out.println();
+//       }
+        int a = 5;
+        System.out.println(a*2);
+    }
+    }
+
